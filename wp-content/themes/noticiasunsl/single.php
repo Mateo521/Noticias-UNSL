3867854ec83f3106207e6c55677bff6f379e07aa
Mateo521 <mateovillalva42@gmail.com>
2023-11-08 11:38:36 -0300

    foobox

--- wp-content/themes/noticiasunsl/single.php
+++ wp-content/themes/noticiasunsl/single.php
@@ -10,13 +10,13 @@ if (have_posts()) :
 
             <div class="w-full py-12">
                 <div class="flex justify-center">
-                    <div class="max-w-screen-md w-full  p-3">
+                    <div class="max-w-screen-md w-full  p-3" id="noticia">
                         <div class="flex items-center gap-3">
                             <button onclick="tipografia();">Dislexia</button>
                             <button onclick="blancoynegro();">Blanco y negro</button>
                             <button onclick="sintesisdevoz();">Síntesis de voz</button>
                         </div>
-                        <header class="entry-header" id="noticia">
+                        <header class="entry-header">
                             <h1 class="text-4xl"><?php the_title(); ?></h1>
                         </header>
                         <?php
@@ -76,6 +76,32 @@ endif;
 
 
     }
+
+
+/*
+    jQuery(document).ready(function($) {
+        console.log("aa");
+        // Selecciona todas las imágenes en el contenido
+        $('#noticia img').each(function() {
+            // Obtiene el contenedor padre de la imagen
+            var parentFigure = $(this).closest('figure.wp-block-gallery');
+
+            // Obtiene la URL de la imagen
+            var imgSrc = $(this).attr('src');
+            // Crea un enlace alrededor de la imagen
+            var imgLink = $('<a href="' + imgSrc + '" class="fbx-link" />');
+
+            // Verifica si la imagen está dentro de un contenedor con la clase wp-block-gallery
+            if (parentFigure.length > 0) {
+                // Si está dentro de una galería, agrega el atributo rel="gallery" al enlace
+                imgLink.attr('rel', 'gallery');
+            }
+
+            // Envuelve la imagen con el enlace
+            $(this).wrap(imgLink);
+        });
+    });
+    */
 </script>
 
 <style>
@@ -92,6 +118,18 @@ endif;
 
     .tipog {
         font-family: opendyslexic;
+    }
+
+    header h1 {
+        padding: 15px 0;
+    }
+
+    #noticia p {
+        padding: 10px 0 !important;
+    }
+
+    #noticia a {
+        color: blue;
     }
 
     figure img {
